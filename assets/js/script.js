ec1ba1ad31f0e855dbafaf7ac286a8b2855933cd
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.attlocal.net>
2023-12-21 13:45:37 -0500

    added starter variables

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Create variables for easy grabbing sections in HTML using JQuery
+var inputSearch = $(".search");
+var searchBtn = $("#searchBtn");
+var searchHistory = $("#searchHistory");
+
+var currentCity = $(".currentCity");
+var currentDate = $(".currentDate");
+var currentTemp = $(".currentTemp");
+var currentHumidity = $(".currentHumidity");
+var currentWind = $(".currentWind");
+var currentIcon = $(".mainWeatherIcon");
+
+var futureDate = $(".card");
+
+// Create a variable for the API key
+var apiKey = "e5d2070e2cf8c17bde06a4eba2c18e7f";
